b3f3c7a00301868e4e2faddb27703bd2d2d22134
Johnnathan Baxter <jbaxter6@radford.edu>
2020-05-14 20:21:22 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ def find_min_in_nested_arrays(src)
 small_num_array = []
 row_index = 0
 
-    while row_index < src.count do
+    while src[row_index] < src.count do
       
       small_num_array << src.row_index.min
       
